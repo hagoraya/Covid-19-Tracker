eaebc37ee96d71b73655acc77e7bd64631f44242
Harry <harpreetgoraya95@gmail.com>
2020-04-15 17:04:58 -0400

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+
+//Bootstrap Components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
+
+
+
+//My components
 import Main from './components/main.component'
 
 function App() {
   return (
-    <div>
+
+    <Container>
+      <Row className="justify-content-md-center">
+        <h1>Covid-19 Data</h1>
+      </Row>
       <Main></Main>
-    </div>
+    </Container>
+
+
+
+
   );
 }
 
